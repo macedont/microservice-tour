fff86a26cc2fdeec2ac379c71ffd53241df1606b
Macedo <cleitonrt3@gmail.com>
2021-08-11 21:29:01 -0300

    criando um map de promoções

--- src/main/kotlin/com/acme/tour/TourApplication.kt
+++ src/main/kotlin/com/acme/tour/TourApplication.kt
@@ -1,10 +1,25 @@
 package com.acme.tour
 
+import com.acme.tour.model.Promocao
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.Bean
+import java.util.concurrent.ConcurrentHashMap
 
 @SpringBootApplication
-class TourApplication
+class TourApplication{
+	companion object{
+		val initialPromocoes = arrayOf(
+			Promocao(1, "cidade maravilhosa", "Rio de Janeiro", true, 7, 2200.00),
+			Promocao(2, "friozinho dos bom", "Gramados", true, 2, 1400.00),
+			Promocao(3, "a cidade fotogenica", "Tokyo", false, 7, 12200.00),
+			Promocao(4, "cidade do sol", "Teresina", true, 3, 1500.00),
+		)
+	}
+
+	@Bean
+	fun promocoes() = ConcurrentHashMap<Long, Promocao>(initialPromocoes.associateBy(Promocao::id))
+}
 
 fun main(args: Array<String>) {
 	runApplication<TourApplication>(*args)
--- src/main/kotlin/com/acme/tour/controller/PromocaoController.kt
+++ src/main/kotlin/com/acme/tour/controller/PromocaoController.kt
@@ -1,15 +1,18 @@
 package com.acme.tour.controller
 
 import com.acme.tour.model.Promocao
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestMethod
 import org.springframework.web.bind.annotation.RestController
+import java.util.concurrent.ConcurrentHashMap
 
 @RestController
 class PromocaoController {
+
+    @Autowired
+    lateinit var promocoes: ConcurrentHashMap<Long, Promocao>
+
     @RequestMapping(value = ["/promocoes"], method = arrayOf(RequestMethod.GET))
-    fun getPromocao(): Promocao{
-        var promocao = Promocao(1, "oi", "teresina", true, 22, 4200.99)
-        return promocao
-    }
+    fun getPromocao() = promocoes
 }
